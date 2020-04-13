3bb1d60c7e62438ff6bb3284d20495a89b63726d
James Lenahan <james.k.lenahan@gmail.com>
2020-04-13 20:06:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('main').remove();
+
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Tony is the champion';
